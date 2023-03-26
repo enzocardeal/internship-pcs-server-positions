f0dc1a4cb32903aba59bed2f6028385e4a7f8535
Enzo Neves <enzocardeal@usp.br>
2023-03-25 22:05:16 -0300

    feat(pubsub-integration) pub message when creating or deleting application

--- src/main/java/com/poli/internship/data/datasource/ApplicationDataSource.java
+++ src/main/java/com/poli/internship/data/datasource/ApplicationDataSource.java
@@ -1,16 +1,22 @@
 package com.poli.internship.data.datasource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poli.internship.api.error.CustomError;
 import com.poli.internship.data.entity.ApplicationEntity;
 import com.poli.internship.data.entity.PositionEntity;
 import com.poli.internship.data.mapper.ApplicationMapper;
+import com.poli.internship.data.messaging.PubsubOutboundGateway;
 import com.poli.internship.data.repository.ApplicationRepository;
 import com.poli.internship.data.repository.PositionRepository;
+import com.poli.internship.domain.models.CurriculumAuthorizationActionType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.execution.ErrorType;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import static com.poli.internship.domain.models.ApplicationModel.Application;
+import static com.poli.internship.domain.models.ApplicationModel.ApplicationMessage;
 
 @Service
 public class ApplicationDataSource {
@@ -18,6 +24,8 @@ public class ApplicationDataSource {
     public ApplicationRepository applicationRepository;
     @Autowired
     public PositionRepository positionRepository;
+    @Autowired
+    private PubsubOutboundGateway messagingGateway;
 
     public Application createApplication(String positionId, String userId){
         if(
@@ -29,6 +37,20 @@ public class ApplicationDataSource {
             ApplicationEntity applicationEntity = applicationRepository.save(
                     new ApplicationEntity(positionEntity, Long.parseLong(userId))
             );
+            try{
+                ApplicationMessage applicationMessage = new ApplicationMessage(
+                        CurriculumAuthorizationActionType.GRANT,
+                        Long.toString( positionEntity.getUserId()),
+                        userId
+                        );
+
+                String message = (new ObjectMapper()).writeValueAsString(applicationMessage);
+                messagingGateway.sendToPubsub(message);
+            } catch (Exception e){
+                applicationRepository.delete(applicationEntity);
+
+                throw new CustomError("Application creation failed.", ErrorType.INTERNAL_ERROR);
+            }
             return ApplicationMapper.INSTANCE.aplicationEntityToModel(applicationEntity);
         }
         return null;
@@ -36,7 +58,23 @@ public class ApplicationDataSource {
 
     public Boolean deleteApplication(String id){
         if(applicationRepository.existsById(Long.parseLong(id))){
+            ApplicationEntity applicationEntity = applicationRepository.findById(Long.parseLong(id));
+            String companyId = applicationEntity.getPosition().getUserId().toString();
             applicationRepository.deleteById(Long.parseLong(id));
+
+            try{
+                ApplicationMessage applicationMessage = new ApplicationMessage(
+                        CurriculumAuthorizationActionType.REVOKE,
+                        companyId,
+                        Long.toString(applicationEntity.getUserId())
+                );
+
+                String message = (new ObjectMapper()).writeValueAsString(applicationMessage);
+                messagingGateway.sendToPubsub(message);
+            } catch (Exception e){
+                applicationRepository.save(applicationEntity);
+                throw new CustomError("Application deletion failed.", ErrorType.INTERNAL_ERROR);
+            }
             return true;
         }
         return false;
--- src/main/java/com/poli/internship/domain/models/ApplicationModel.java
+++ src/main/java/com/poli/internship/domain/models/ApplicationModel.java
@@ -4,4 +4,5 @@ import static com.poli.internship.domain.models.PositionModel.Position;
 
 public class ApplicationModel {
     public static record Application(String id, Position position, String userId){};
+    public static record ApplicationMessage(CurriculumAuthorizationActionType action, String companyId, String studentId){};
 }
